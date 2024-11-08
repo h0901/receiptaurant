2ff50d3a6cc532b39f5fe41f46e42332c8ae8a90
Huma Hussain <hhussain0901@Humas-MacBook-Air.local>
2024-11-08 16:46:30 -0600

    Modified SSO Authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
-import UploadReceipt from "./frontend/components/UploadReceipt";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { VITE_CLERK_PUBLISHABLE_KEY } from "./frontend/apis/config";
+import RoutesConfig from "./frontend/components/RoutesConfig";
+
+const clerkPublishableKey = VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
   return (
-    <>
-      <h1>Receiptaurant</h1>
-      <UploadReceipt />
-    </>
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      <Router>
+        <h1>Receiptaurant</h1>
+        <RoutesConfig />
+      </Router>
+    </ClerkProvider>
   );
 }
 
--- client/src/frontend/apis/config.ts
+++ client/src/frontend/apis/config.ts
@@ -1,4 +1,7 @@
 export const GOOGLE_API_KEY = "AIzaSyA7gbee1inBPOpvbkVSezjM0R-lnntjG1w";
-export const API_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GOOGLE_API_KEY}`
+export const API_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GOOGLE_API_KEY}`;
 export const API_PROMPT =
-  "Use the attached image and return a JSON object containing restaurant name, date, subtotal, total, surcharges array with list of different surcharges in the given format surcharge name, percentage of each surcharge named as surcharge percentage and surcharge value , and taxes:\n";
+  "Use the attached image and return a JSON object containing restaurant name, date, subtotal, total, surcharges (structure format with surcharge name and value), and taxes:\n";
+
+export const VITE_CLERK_PUBLISHABLE_KEY =
+  "pk_test_aG9wZWZ1bC1hbnQtNTYuY2xlcmsuYWNjb3VudHMuZGV2JA";
