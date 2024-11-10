42c76f3e6ecab9120b9aacf35f8243c39a45896c
Huma Hussain <hhussain0901@Humas-MacBook-Air.local>
2024-11-10 14:35:12 -0600

    Deploying server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const restaurantRoutes = require('./routes/restaurant.js');
-const billRoutes = require('./routes/bill.js');
-const surchargeRoutes = require('./routes/surcharges.js');
+const restaurantRoutes = require("./routes/restaurant.js");
+const billRoutes = require("./routes/bill.js");
+const surchargeRoutes = require("./routes/surcharges.js");
 
 const app = express();
 const PORT = 8080;
 const corsOptions = {
-  origin: ["http://localhost:5173"]
+  origin: ["https://cs484-final-project-server.vercel.app"], //["http://localhost:5173"]
 };
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.use('/api/restaurant', restaurantRoutes);
-app.use('/api/bill', billRoutes);
-app.use('/api/surcharge', surchargeRoutes);
+app.use("/api/restaurant", restaurantRoutes);
+app.use("/api/bill", billRoutes);
+app.use("/api/surcharge", surchargeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
