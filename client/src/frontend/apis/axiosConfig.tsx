f90bafae8d22a07103ba7108ea9f19c5565c04ef
Bhavyashree P <71510125+Bhavyashreeputta@users.noreply.github.com>
2024-11-10 22:51:41 -0600

    Update axiosConfig.tsx

--- client/src/frontend/apis/axiosConfig.tsx
+++ client/src/frontend/apis/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendApi = axios.create({
-  baseURL: "https://cs484-final-project-server.vercel.app/api", //'http://localhost:8080/api',
+  baseURL: "https://receiptaurant.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
