cd3cf70df8e48388f403b2db475cd60496c7d1da
Huma Hussain <hhussain0901@Humas-MacBook-Air.local>
2024-11-11 09:37:22 -0600

    Optimized api functions

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const restaurantRoutes = require("./routes/restaurant.js");
-const billRoutes = require("./routes/bill.js");
-const surchargeRoutes = require("./routes/surcharges.js");
+const restaurantRoutes = require("./api/restaurant.js");
+const billRoutes = require("./api/bill.js");
+const surchargeRoutes = require("./api/surcharges.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const corsOptions = {
-  origin: ["http://localhost:5173"]
+  origin: ["http://localhost:5173", "https://cs484-final-project.vercel.app"],
 };
 
 app.use(cors(corsOptions));
@@ -19,5 +19,5 @@ app.use("/api/bill", billRoutes);
 app.use("/api/surcharge", surchargeRoutes);
 
 app.listen(PORT, () => {
-   console.log(`Server is running on http://localhost:${PORT}`);
- });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
