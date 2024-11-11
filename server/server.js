eaf281f17924f3a960c4cee91fc5f3a80dc661cd
Bhavyashree P <71510125+Bhavyashreeputta@users.noreply.github.com>
2024-11-10 23:20:14 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const surchargeRoutes = require("./routes/surcharges.js");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const corsOptions = {
-  origin: ["https://cs484-final-project.onrender.com"],
+  origin: ["https://receiptaurant.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
