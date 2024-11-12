71cbfafdd6cc28ba87b38b23915f5c6860e41ed6
Huma Hussain <bhavyasree2002@gmail.com>
2024-11-12 14:57:20 -0600

    Deploying server

--- client/src/frontend/apis/axiosConfig.tsx
+++ client/src/frontend/apis/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendApi = axios.create({
-  baseURL: "https://cs484-final-project.vercel.app/api",
+  baseURL: "https://receiptaurant-server.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
