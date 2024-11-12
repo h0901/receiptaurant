031cb634f7af6c8f7a3a0e64c284080d7d82f368
Huma Hussain <bhavyasree2002@gmail.com>
2024-11-12 15:00:13 -0600

    Deploying client

--- server/server.js
+++ server/server.js
@@ -8,10 +8,13 @@ const surchargeRoutes = require("./routes/surcharges.js");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const corsOptions = {
-  origin: ["https://receiptaurant.vercel.app", "https://cs484-final-project.vercel.app"], 
+  origin: [
+    "https://receiptaurant-delta.vercel.app",
+    "https://receiptaurant-server.vercel.app/",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, 
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -22,7 +25,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
 
-
 app.use("/api/restaurant", restaurantRoutes);
 app.use("/api/bill", billRoutes);
 app.use("/api/surcharge", surchargeRoutes);
@@ -30,4 +32,3 @@ app.use("/api/surcharge", surchargeRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
